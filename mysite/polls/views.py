2038d0be9769ceff7553c928b236a98c1787a737
rafaelsanchezsalado <rafael.sanchez.salado@gmail.com>
2020-09-03 19:43:12 +0200

    creating 404 and templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 from django.http import HttpResponse
 from django.template import loader
@@ -10,7 +11,8 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})       
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
