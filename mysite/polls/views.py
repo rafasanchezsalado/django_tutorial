3371919e4aafc5c7aa8c4a8648ca78813cdb5e03
rafaelsanchezsalado <rafael.sanchez.salado@gmail.com>
2020-09-03 18:20:31 +0200

    templates folder created and index view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 
 def index(request):
@@ -15,3 +18,8 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
