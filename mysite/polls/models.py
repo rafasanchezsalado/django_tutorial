85db4e727893f43019bc705235cfea39a84b4488
rafaelsanchezsalado <rafael.sanchez.salado@gmail.com>
2020-09-09 01:25:29 +0200

    changes in serializer and models

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=500)
     pub_date = models.DateTimeField("date published")
 
     def __str__(self):
@@ -22,7 +22,7 @@ class Question(models.Model):
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=500)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
--- mysite/polls/serializers.py
+++ mysite/polls/serializers.py
@@ -3,12 +3,21 @@ from .models import Question, Choice
 
 
 class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.HyperlinkedRelatedField(
+        many=False, view_name="question-detail", read_only=True
+    )
+    pub_date = serializers.DateTimeField(format="%Y-%m-%d")
+
     class Meta:
         model = Question
         fields = ["id", "question_text", "pub_date"]
 
 
 class ChoiceSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.HyperlinkedRelatedField(
+        many=False, view_name="choice-detail", read_only=True
+    )
+
     class Meta:
         model = Choice
         fields = ["id", "question", "choice_text", "votes"]
